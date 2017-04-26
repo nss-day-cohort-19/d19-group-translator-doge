10e52566d3fec2904886233ad73282b38f60f089
Rebecca Wilkerson <rueTH@users.noreply.github.com>
2017-04-26 11:57:37 -0500

    pivoted to prioritize MVP

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,16 +1,49 @@
 "strict";
 
-//btn "click" event listener
-//phrase select dropdown event listener (.value)
-//lang select dropdown event listener (.value)
+var output = {};
+
+var btn = document.getElementById('trans-btn');
+  console.log(btn);
+var textInput = document.getElementById('userInput');
+  console.log(textInput);
+var lang = document.getElementById('lang');
+  console.log(lang);
+var outputBox = document.getElementById('outputText');
+
+//event listeners
+btn.addEventListener("click", checkLang)
 
 
-//"Text String"
-// function noname
-//var Translate = userInput.value
-  // if select checked =
-  // run teamMember[i] function
-  // else if (select checked
-  // run teamMember2 function
+function checkLang(e){
+  console.log('checkLang wants to run real bad');
+  var textValue = "";
+  textValue = textInput.value.toLowerCase();
+  console.log('text input: ', textValue);
+  var langValue = "";
+  langValue = lang.value;
+  console.log('language: ', langValue);
 
-  ///.innerhtml
+  switch (langValue) {
+
+    case "doge":
+    Translator.getDoge(textValue);
+    break;
+
+    case "german":
+    Translator.getGerman(textValue);
+    break;
+
+    case "spanish":
+    Translator.getSpanish(textValue);
+    break;
+
+    default:
+    alert("WHY YOU NO SELECT LANGUAGE?");
+  }
+}
+
+function printToDom(outputString) {
+  output.innerHTML = outputString;
+}
+
+ // if(lang.option === 'doge')
